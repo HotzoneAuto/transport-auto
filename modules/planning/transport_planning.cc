b1a77df519d6a5cabb63be405f447a7743425776
sunzengpeng <2285438399@qq.com>
2020-12-31 15:27:34 +0800

    add planning

---
+++ modules/planning/transport_planning.cc
@@ -0,0 +1,139 @@
+#include "transport_planning.h"
+
+bool transport_Planning::Init() {
+  AINFO << "Transport_Control init";
+  if (!GetProtoConfig(&planning_setting_conf_)) {
+    AERROR << "Unable to load conf file" << ConfigFilePath();
+    return false;
+  }
+  if (!planning_setting_conf_.trajmode()) {
+    file_csv.open_file(fname);
+    std::string msg_w = "frame,pgsnh,gpsnl,gpseh,gpsel,heading_angle,"
+                        "yaw_rate,gps_state,gps_velocity,acceleration_forward,"
+                        "acceleration_lateral,acceleration_down,pitch_angle,"
+                        "velocity_down,velocity_lateral,velocity_forward,roll_angle";
+    file_csv.write_file(msg_w);
+    frame = 0;
+  } else {
+    ReadTraj();
+  }
+  return true;
+}
+
+void transport_Planning::ReadTraj() {
+  //读取所有点的经纬度
+  traj_record_file.open(fname, std::ios::in);
+  char linestr[500] = {0};
+	traj_record_file.getline(linestr,500);
+	while (traj_record_file.getline(linestr, 500)) {
+		std::stringstream ss(linestr);
+		std::string csvdata[17];
+		double gpsnh,gpsnl,gpseh,gpsel,velocity;
+		for (int i = 0; i < 17; i++) {
+			char tempdata[500] = {0};
+			ss.getline(tempdata,500,',');
+			csvdata[i] = std::string(tempdata);
+		}
+    gpsnh = atof(csvdata[1].data());
+		gpsnl = atof(csvdata[2].data());
+    gpseh = atof(csvdata[3].data());
+    gpsel = atof(csvdata[4].data());
+    velocity = atof(csvdata[8].data());
+    trajinfo[0].push_back(gpsnh);
+    trajinfo[1].push_back(gpsnl);
+    trajinfo[2].push_back(gpseh);
+    trajinfo[3].push_back(gpsel);
+    trajinfo[4].push_back(velocity);
+	}
+  traj_record_file.close();
+}
+
+
+void transport_Planning::UpdateTraj(const std::shared_ptr<Gps>& msg0) {
+  //将靠近的若干个点转移到车辆的坐标系中
+  //寻找轨迹上一段范围内的距离本车最近的点
+  double N_now = msg0->gpsnl() + msg0->gpsnh();
+  double E_now = msg0->gpsel() + msg0->gpseh();
+  double Azi_now = msg0->heading_angle() / 180 * M_PI;
+  int lastindex = TrajIndex;
+  double min_dis = MAXDIS;
+  for (int i = lastindex;
+       i < std::min(lastindex + TRAJLENGTH, (int)trajinfo[0].size()); i++) {
+    double N_point = trajinfo[0][i] + trajinfo[1][i];
+    double E_point = trajinfo[3][i] + trajinfo[4][i];
+    double dis = apollo::drivers::gps::SphereDis(E_now, N_now, E_point, N_point);
+    if (dis < min_dis) {
+      min_dis = dis;
+      TrajIndex = i;
+    }
+  }
+  //将该点附近的若干个点加入到自车坐标系中
+  rel_loc[0].clear();
+  rel_loc[1].clear();
+  rel_loc[2].clear();
+  for (int i = TrajIndex;
+       i < std::min(TrajIndex + TRAJLENGTH, (int)trajinfo[0].size()); i++) {
+    double N_point = trajinfo[0][i] + trajinfo[1][i];
+    double E_point = trajinfo[2][i] + trajinfo[3][i];
+    double dis = apollo::drivers::gps::SphereDis(E_now, N_now, E_point, N_point);
+    double azi = apollo::drivers::gps::SphereAzimuth(E_now, N_now, E_point, N_point);
+    double rel_x = dis * cos(azi - Azi_now);
+    double rel_y = dis * sin(azi - Azi_now);
+    double vel = trajinfo[4][i];
+    rel_loc[0].push_back(rel_x);
+    rel_loc[1].push_back(rel_y);
+    rel_loc[2].push_back(vel);
+  }
+}
+int transport_Planning::FindLookahead(double totaldis) {
+  double dis = 0;
+  int i = 1;
+  for (i = 1; i < rel_loc[0].size(); i++) {
+    double dx = (rel_loc[0][i] - rel_loc[0][i - 1]);
+    double dy = (rel_loc[1][i] - rel_loc[1][i - 1]);
+    dis += sqrt(dx * dx + dy * dy);
+    if (dis > totaldis) {
+      break;
+    }
+  }
+  return i;
+}
+
+/*
+  Reader Callback function
+*/
+bool transport_Planning::Proc(const std::shared_ptr<Gps>& msg0) {
+  if (!planning_setting_conf_.trajmode()) {
+    if (frame == 65535) {
+      frame = 0;
+    }
+    frame ++;
+
+    std::string msg_w = std::to_string(frame) + "," + std::to_string(msg0->gpsnh()) 
+                    + "," + std::to_string(msg0->gpsnl()) 
+                    + "," + std::to_string(msg0->gpseh()) 
+                    + "," + std::to_string(msg0->gpsel()) 
+                    + "," + std::to_string(msg0->heading_angle()) 
+                    + "," + std::to_string(msg0->yaw_rate()) 
+                    + "," + std::to_string(msg0->gps_state()) 
+                    + "," + std::to_string(msg0->gps_velocity())
+                    + "," + std::to_string(msg0->acceleration_forward()) 
+                    + "," + std::to_string(msg0->acceleration_lateral()) 
+                    + "," + std::to_string(msg0->acceleration_down()) 
+                    + "," + std::to_string(msg0->pitch_angle()) 
+                    + "," + std::to_string(msg0->velocity_down()) 
+                    + "," + std::to_string(msg0->velocity_lateral()) 
+                    + "," + std::to_string(msg0->velocity_forward()) 
+                    + "," + std::to_string(msg0->roll_angle());
+    file_csv.write_file(msg_w);
+  }
+
+  UpdateTraj(msg0);
+  return true;
+}
+
+void transport_Planning::Clear() {
+  if (!planning_setting_conf_.trajmode()) {
+    file_csv.close_file();
+  }
+}
---
+++ modules/planning/transport_planning.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <cstdio>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
+#include "cyber/cyber.h"
+#include "cyber/component/component.h"
+#include "cyber/component/timer_component.h"
+#include "modules/drivers/gps/GPSproto.h"
+#include "modules/drivers/gps/proto/gps.pb.h"
+#include "modules/common/file/file.h"
+#include "modules/planning/proto/planning_setting_conf.pb.h"
+
+#define TRAJLENGTH 200
+#define MAXDIS 99999
+#define L 2.4
+
+using apollo::drivers::Gps;
+using apollo::cyber::Component;
+using apollo::cyber::ComponentBase;
+
+class transport_Planning : public apollo::cyber::Component<Gps> {
+ public:
+  bool Init() override;
+  bool Proc(const std::shared_ptr<Gps>& msg0) override;
+  void Clear() override;
+
+ private:
+  void ReadTraj();
+  void UpdateTraj(const std::shared_ptr<Gps>& msg0);
+  int FindLookahead(double totaldis);
+
+  std::vector<double> trajinfo[6];
+  std::vector<double> rel_loc[3];
+  
+  apollo::common::file::File file_csv;
+  std::fstream traj_record_file;
+  int TrajIndex;
+  int frame;
+  std::string fname = "/apollo/modules/planning/data/gps_record.csv";
+  apollo::planning::PlanningSettingConf planning_setting_conf_;
+};
+CYBER_REGISTER_COMPONENT(transport_Planning)
